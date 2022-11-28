90ace8c2a75448cc77820d83621d2a0e9fb67366
Natthawan31 <Natthawan.p@kkumail.com>
2022-11-28 16:34:07 +0700

    Natthawan

---
+++ Lab01/Lab01/Program.cs
@@ -0,0 +1,4 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Natthawan Puangmalai");
+Console.WriteLine("653450089-1");
+Console.WriteLine("Natthawan.p@kkumail.com");
